fb6d286c513d2de76fb5869740e2dd4c86f7dc0c
Bangyno <bangyno@gmail.com>
2017-12-22 17:49:33 +0800

    1. Add function in html and ts 2. Create and generate android project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var MyCordovaPlugin;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +11,21 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  echoNative(){
+    MyCordovaPlugin.echo(['a', 123, 'b', 456], result => {
+      //理論上不會跑
+      console.log(result);
+      alert(result);
+    }, error => alert(error))
+  }
+
+  getDateNative() {
+    MyCordovaPlugin.getDate(result => {
+      console.log(result);
+      alert(result);
+    }, error => alert(error))
   }
 
 }
